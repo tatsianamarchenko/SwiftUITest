c97cdbc98d7dc85a87cc38d272473a111593bc55
tatsiana <tanya3308225@mail.ru>
2022-01-10 18:03:53 +0300

    Initial Commit

---
+++ ui swift app/ui_swift_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ui_swift_appApp.swift
+//  ui swift app
+//
+//  Created by Tatsiana Marchanka on 10.01.22.
+//
+
+import SwiftUI
+
+@main
+struct ui_swift_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
